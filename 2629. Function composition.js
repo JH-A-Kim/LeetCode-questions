00a3bb01a03ee3e6dbd28644e7933e4d28aef69b
jak28 <jak28@sfu.ca>
2024-04-06 13:19:55 +0800

    66 ms, 49.77 mb

---
+++ 2629. Function composition.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    return function(x) {
+        if(functions.length===0){
+            return x;
+        }
+        let result = x;
+        for (let i=functions.length-1; i>=0; i--){
+            result=functions[i](result);
+        }
+        return result;
+    }
+};
+
+
+
+ const fn = compose([x => x + 1, x => 2 * x])
+
+
+console.log(fn(4))
