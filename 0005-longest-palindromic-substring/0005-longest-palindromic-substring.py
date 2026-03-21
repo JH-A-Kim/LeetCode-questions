0239cfc538a7a886a6c5ae72eabaa425ba99f6cb
Jung-Hyun Andrew Kim <jhakimcs03@outlook.com>
2026-03-21 15:24:32 -0700

    Time: 227 ms (83.49%), Space: 19.3 MB (85.88%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def expand(s, L, R):
+        def expand(s, L, R): # expand function will expand around center by taking a left and right input start as well as a string. After that it checks using odd logic and left logic from the inputs. it does a while loop from the center and the moment things do not equal it will return the substring palindrome
             while L >=0 and R < len(s) and s[L] == s[R]:
                 L-=1
                 R+=1
@@ -8,7 +8,7 @@ class Solution:
 
         best = ""
 
-        for i in range(len(s)):
+        for i in range(len(s)): ## after we return the substring palindromes for each value in the array moving from the center we simply check which substring is bigger than best. This overall is an application of 2 pointer but from the inside out not the outside in.
             odd = expand(s, i, i)
             even = expand(s, i, i+1)
             if len(odd) > len(best):
@@ -17,4 +17,5 @@ class Solution:
                 best = even
 
         return best
+        
                 
