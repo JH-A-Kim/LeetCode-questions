6eda80f16d2be0055e6fadf64fcd694ee84de14c
Jung-Hyun Andrew Kim <jhakimcs03@outlook.com>
2026-09-19 07:39:55 -0700

    Time: 1 ms (12.33%), Space: 19.4 MB (6.06%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        ## number of rows is numRows the number of rows is also the number of values in that row. Each value is the sum of the 2 values above it. Each edge value is always one. But how does that look like in an array. What is the smallest subproblem i can find. To find the value its always the sum of the previous arrays values at the same index and previous index I have to start off with generating an 2 x 2 array. 
+        pascalsTriangle=[]
+        row=0
+        while row<numRows:
+            arrayRow=[]
+            col=0
+            while col < row+1:
+                if col == 0 or col==row:
+                    arrayRow.append(1)
+                    print(row)
+                    print(col)
+                    print(arrayRow)
+                else:
+                    arrayRow.append(pascalsTriangle[row-1][col]+pascalsTriangle[row-1][col-1])
+                    print(row)
+                col+=1
+            pascalsTriangle.append(arrayRow)
+            row+=1
+
+        return pascalsTriangle
+                
+        
